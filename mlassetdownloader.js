a223ae5cf3724eda370694c083fbdbb439fd16ed
Christian Relling <jcrelling@gmail.com>
2023-02-18 13:01:05 -0300

    2 version

--- mlassetdownloader.js
+++ mlassetdownloader.js
@@ -7,8 +7,11 @@ function downloadAssets() {
         //console.log(imagesList); //debug
         $.map( imagesList, function( n ) {
             MLurl = n.split('/').pop();
+            url = 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/'+MLurl+'/2400';
+            FileName = MLurl+'.jpg';
+            downloadFile(url, FileName);
             //listDownload.push('https://cdn.download.ams.birds.cornell.edu/api/v1/asset/'+MLurl+'/1200');
-            listDownload.push({FileName: MLurl+'.jpg', URL: 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/'+MLurl+'/1200'});
+            //listDownload.push({FileName: MLurl+'.jpg', URL: 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/'+MLurl+'/1200'});
             //console.log(imageListDownload); //debug
         });
     };
@@ -40,54 +43,39 @@ function downloadAssets() {
 
     console.log(listDownload);
 
-    $.map( listDownload, function( asset ) {
-      console.log('dentro del map')
-      fetch(asset.URL, { method: 'get', mode: 'no-cors', referrerPolicy: 'no-referrer' })
-      .then(res => res.blob())
-      .then(res => {
-        const aElement = document.createElement('a');
-        aElement.setAttribute('download', asset.FileName);
-        const href = URL.createObjectURL(res);
-        aElement.href = href;
-        aElement.setAttribute('target', '_blank');
-        aElement.click();
-        URL.revokeObjectURL(href);
-      });
-    });
-    //download_files(listDownload);
 };
 
 
 
+function downloadFile(url, fileName) {
+  fetch(url, { method: 'get', referrerPolicy: 'no-referrer' })
+    .then(res => {
+        console.log(res)
+      })
+    .then(res => res.blob())
+    .then(res => {
+      const aElement = document.createElement('a');
+      aElement.setAttribute('download', fileName);
+      const href = URL.createObjectURL(res);
+      console.log(href)
+      aElement.href = href;
+      aElement.setAttribute('target', '_blank');
+      aElement.click();
+      URL.revokeObjectURL(href);
+    });
+};
 
 
-  function download_files(files) {
-    function download_next(i) {
-      if (i >= files.length) {
-        return;
-      }
-      var a = document.createElement('a');
-      a.href = files[i];
-      a.target = '_blank';
-      // Use a.download if available, it prevents plugins from opening.
-      if ('download' in a) {
-        a.download = files[i].filename;
-      }
-      // Add a to the doc for click to work.
-      (document.body || document.documentElement).appendChild(a);
-      if (a.click) {
-        a.click(); // The click method is supported by most browsers.
-      } else {
-        $(a).click(); // Backup using jquery
-      }
-      // Delete the temporary link.
-      a.parentNode.removeChild(a);
-      // Download the next file with a small timeout. The timeout is necessary
-      // for IE, which will otherwise only download the first file.
-      setTimeout(function() {
-        download_next(i + 1);
-      }, 500);
-    }
-    // Initiate the first download.
-    download_next(0);
-  }
+async function toDataURL(url) {
+  const blob = await fetch(url).then(res => res.blob());
+  return URL.createObjectURL(blob);
+}
+
+async function download(imageURL, filename) {
+  const a = document.createElement("a");
+  a.href = await toDataURL(imageURL);
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
